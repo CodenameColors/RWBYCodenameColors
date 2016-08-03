88f2781de2dc12b11cab07b5528d4a6d9473345d
antonio morales <calling911x@gmail.com>
2016-08-03 18:51:17 -0400

    Synced up 2.5D Shooting
    However it is wrong. Commiting just incase

--- RWBY_CodenameColors/RWBY_CodenameColorsCharacter.h
+++ RWBY_CodenameColors/RWBY_CodenameColorsCharacter.h
@@ -112,7 +112,7 @@ protected:
 	void SwitchCamera();
 
 	//method used to fire from the center of the camera (screen)
-	void OnFire();
+	virtual void OnFire();
 
 	virtual void OnDodge();
 
@@ -123,10 +123,10 @@ protected:
 	void StopHealing();
 
 	//method used start shooting
-	void StartShooting();
+	virtual void StartShooting();
 
 	//method used to stop shooting
-	void StopShooting();
+	virtual void StopShooting();
 
 	//method used to start dodging
 	virtual void StartDodging();
@@ -139,7 +139,7 @@ protected:
 	virtual void PerformDodge(bool bDodge);
 
 	//method used to preform tasks (Client)
-	void PerformTask(ETask::Type NewTask);
+	virtual void PerformTask(ETask::Type NewTask);
 
 	void PerformUseDust( );
 
