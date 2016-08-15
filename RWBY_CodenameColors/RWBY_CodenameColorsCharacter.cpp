612279c62ca634fe167d41b1640f27f9898a3c2d
antonio morales <calling911x@gmail.com>
2016-08-15 16:32:03 -0400

    Revert "More Backups"
    This reverts commit a3de385c6a4dd0a7e009bac835875992dab2ba40.

--- RWBY_CodenameColors/RWBY_CodenameColorsCharacter.cpp
+++ RWBY_CodenameColors/RWBY_CodenameColorsCharacter.cpp
@@ -783,7 +783,6 @@ void ARWBY_CodenameColorsCharacter::ServerAddVelocity_Implementation() {
 			ThisPlayer->GetCharacter()->GetCharacterMovement()->AddImpulse(JumpAngle * 7, true);
 			//ThisPlayer->GetCharacter()->GetCharacterMovement()->Velocity = ThisPlayer->GetCharacter()->GetActorForwardVector() *-500;
 		}
-		//
 		bWallJumping = false;
 	}
 }
